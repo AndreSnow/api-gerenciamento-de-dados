063d15125cb2725a164dd2b71f02b40d91414703
andresnow <andre_neve@hotmail.com>
2022-06-13 21:40:49 -0300

    primeiro commit

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Task>
+ */
+class TaskFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name'          => $this->faker->name,
+            'description'   => $this->faker->text,
+            'status'        => $this->faker->numberBetween(0, 2),
+            'file_url'      => $this->faker->imageUrl()
+        ];
+    }
+}
